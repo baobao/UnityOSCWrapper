4edb0e17ca6820d3c4d01129557c4c7459c8f1e0
baobao <shunsuke.ohba@gmail.com>
2020-09-14 03:28:50 +0900

    add Sample

---
+++ Assets/Sample/Sample.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using info.shibuya24.osc;
+using UnityEngine;
+using UnityOSC;
+
+public class Sample : MonoBehaviour
+{
+    private OSCSender _sender;
+    private OSCReceiver _receiver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sender = OSCSender.Create("testClient", 1000, "192.168.11.4");
+
+        _receiver = OSCReceiver.Create("testServer", 1000);
+        _receiver.onListenToOSCMessage += OnListenToOSCMessage;
+    }
+
+    private void OnListenToOSCMessage(OSCPacket obj)
+    {
+        Debug.Log($"{obj.Address} / {obj.Data[0]} / {obj.Data.Count} / {obj.TimeStamp}");
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Send"))
+        {
+            _sender.Send("/aaa", "hogehoge");
+        }
+    }
+
+    void Update()
+    {
+        _receiver.UpdateListen();
+    }
+}
+
+public class Foo
+{
+    public string a = "a";
+    public string b = "b";
+    public int foo = 100;
+}
